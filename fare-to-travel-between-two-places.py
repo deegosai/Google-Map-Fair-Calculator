c5211c1483a6535c4fb389c8a2cd9f7663b542f3
deegosai <114422899+deegosai@users.noreply.github.com>
2024-08-11 19:30:50 +0530

    Update fare-to-travel-between-two-places.py

--- fare-to-travel-between-two-places.py
+++ fare-to-travel-between-two-places.py
@@ -1,7 +1,7 @@
 import requests
 import json
 
-key="AIzaSyDHkUK9MjmEWAQQK5Vyc4dvMkRXbB0t3eU"  ##if there is an error while executing this program "index out of range" just change this key value by generating a new key form the site: https://console.cloud.google.com/apis/credentials?project=myproject-244611 ---> create credentials ---> API key
+key=""  ##if there is an error while executing this program "index out of range" just change this key value by generating a new key form the site: https://console.cloud.google.com/apis/credentials?project=myproject-244611 ---> create credentials ---> API key
 source="Churchgate station"
 destination="Borivali station"
 url="https://maps.googleapis.com/maps/api/distancematrix/json?origins="+source+"&destinations="+destination+"&key="+key
